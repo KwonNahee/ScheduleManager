6cda5a9462a77ea464df648b477d43dabbd37c26
KwonNahee <0523cindy@gmail.com>
2020-04-11 17:15:11 +0900

    initial commit

---
+++ app/src/main/java/com/android/developer/schedulemanager/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android.developer.schedulemanager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
